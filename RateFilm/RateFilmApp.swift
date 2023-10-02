87e2996bd15c3c91fdd96e5e158ca3ff2f04aebd
Kirill <kirill.kazakov26@gmail.com>
2023-10-02 16:01:32 +0300

    Initial Commit

---
+++ RateFilm/RateFilmApp.swift
@@ -0,0 +1,17 @@
+//
+//  RateFilmApp.swift
+//  RateFilm
+//
+//  Created by Кирилл Казаков on 02.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct RateFilmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
