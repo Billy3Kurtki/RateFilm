01b775fad90c781e0f01a43ae98efa3bb2dc580f
Kirill <kirill.kazakov26@gmail.com>
2023-10-11 22:44:38 +0300

    Были добавление Вьюхи-заглушки для основного TabView. Также в HorizontalScrollView добавлен авто-скролл на центр + анимация.

---
+++ RateFilm/Modules/MainModule/ViewModels/ListViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ListViewModel.swift
+//  RateFilm
+//
+//  Created by Кирилл Казаков on 11.10.2023.
+//
+
+import SwiftUI
+
+struct ListViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListViewModel()
+}
