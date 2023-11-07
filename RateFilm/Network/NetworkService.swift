c032505d57b35dc91fbb43c2d5e0e35ad1d8dbc1
Kirill <kirill.kazakov26@gmail.com>
2023-11-07 19:22:19 +0300

    Добавлены 2 метода в NetworkService (fetch и post).

--- RateFilm/Network/NetworkService.swift
+++ RateFilm/Network/NetworkService.swift
@@ -6,8 +6,97 @@
 //
 
 import Foundation
-import UIKit
 
-struct NetworkService {
+protocol NetworkLayer {
+    func fetch<T: Decodable>(urlString: String, onCompletion: @escaping (Result<T, NetworkError>) -> Void)
+    func post<T: Encodable>(urlString: String, body: T, onCompletion: @escaping (Result<Data?, NetworkError>) -> Void)
+}
+
+struct NetworkService: NetworkLayer {
     
+    func fetch<T: Decodable>(urlString: String, onCompletion: @escaping (Result<T, NetworkError>) -> Void) {
+        guard let url = URL(string: urlString) else {
+            onCompletion(.failure(NetworkError.invalidUrl))
+            return
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            if error != nil {
+                onCompletion(.failure(NetworkError.networkError))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                onCompletion(.failure(NetworkError.unexpectedResponse))
+                return
+            }
+            
+            guard Self.httpStatusCodeSuccess.contains(response.statusCode) else {
+                onCompletion(.failure(NetworkError.failedResponse))
+                return
+            }
+            
+            guard let data else {
+                onCompletion(.failure(NetworkError.dataError))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let responseData = try decoder.decode(T.self, from: data)
+                onCompletion(.success(responseData))
+            } catch {
+                onCompletion(.failure(NetworkError.parseError))
+            }
+        }.resume()
+    }
+    
+    func post<T: Encodable>(urlString: String, body: T, onCompletion: @escaping (Result<Data?, NetworkError>) -> Void) {
+        guard let url = URL(string: urlString) else {
+            onCompletion(.failure(NetworkError.invalidUrl))
+            return
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        do {
+            urlRequest.httpBody = try JSONEncoder().encode(body)
+        } catch {
+            onCompletion(.failure(NetworkError.parseError))
+        }
+        
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            if error != nil {
+                onCompletion(.failure(NetworkError.networkError))
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                onCompletion(.failure(NetworkError.unexpectedResponse))
+                return
+            }
+            
+            guard Self.httpStatusCodeSuccess.contains(response.statusCode) else {
+                onCompletion(.failure(NetworkError.failedResponse))
+                return
+            }
+            
+            onCompletion(.success(data))
+        }.resume()
+    }
+    
+    private static let httpStatusCodeSuccess = 200..<300
+}
+
+enum NetworkError: Error {
+    case invalidUrl
+    case networkError
+    case dataError
+    case parseError
+    case unexpectedResponse
+    case failedResponse
 }
